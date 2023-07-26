7865a2a177f0802287ee1036a780b671b95e7a0c
Gui <guilhermesouzanastoo@hotmail.com>
2023-07-26 13:43:39 -0300

    refactor: @ComponentScan annotation adicionada

--- regescweb/src/main/java/br/com/guilherme/regescweb/RegescwebApplication.java
+++ regescweb/src/main/java/br/com/guilherme/regescweb/RegescwebApplication.java
@@ -2,8 +2,10 @@ package br.com.guilherme.regescweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class RegescwebApplication {
 
 	public static void main(String[] args) {
